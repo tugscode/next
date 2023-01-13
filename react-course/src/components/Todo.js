a505dc6d3c7d3ac1afe9f4925a31a4c0189cfb00
Tugs-Erdene Otgonbat <tugscode@tugscodes-MacBook-Air.local>
2023-01-13 19:20:08 +0800

    [ADD] branch

--- react-course/src/components/Todo.js
+++ react-course/src/components/Todo.js
@@ -4,7 +4,6 @@ function Todo(props) {
       <h2>{props.text}</h2>
       <div className="actions">
         <button className="btn">Delete</button>
-        <p>2.29 Handling events </p>
       </div>
     </div>
   );
